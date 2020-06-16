4591cf223d967ce38fe8d675af6530b8ad382196
fresher93 <60925770+fresher93@users.noreply.github.com>
2020-06-16 15:16:00 +0700

    remove all file

--- api/search/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('requesters/search/', views.requesters_search),
-    path('technicals/search/', views.technicals_search),
-]
--- api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-
-urlpatterns = [
-    path('api/', include('api.claims.urls')),
-    path('api/', include('api.authens.urls')),
-    path('api/', include('api.search.urls'))
-]
